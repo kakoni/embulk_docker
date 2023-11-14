e2b1821a5b6c1bb6738022d7ab28e9f186daeb48
Karri Niemelä <kakoni@gmail.com>
2023-11-14 13:48:31 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,8 @@ if [ -n "$PLUGINS" ] ; then
   java -jar /embulk/embulk gem install $PLUGINS
 fi
 
+#Jruby fix for now
+JDK_JAVA_OPTIONS="--add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED"
+export JDK_JAVA_OPTIONS
+
 java -jar /embulk/embulk "$@"
